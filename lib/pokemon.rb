4eda6bb6a3c3e2dd46fe5ce8f9eb1d92141b65e3
github username <github email address>
2017-10-16 17:29:50 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
 class Pokemon
+
+ attr_accessor :name, :type, :db, :id
+
+ def initialize(id: nil, name: 'null', type: 'nil', db: 'nil')
+    @name = name
+    @type = type
+    @db = db
+    @id = id
+
+
+ end
+
+ def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",name, type)
+  end
+
+ def self.find(id, db)
+    sql = "SELECT * FROM pokemon WHERE id = ?"
+        result = (db.execute(sql, id)).flatten
+        new_pokemon = Pokemon.new(id:result[0], name:result[1], type:result[2])
+        new_pokemon
+
+     end
+
+
 end
--- spec/pokemon_spec.rb
+++ spec/pokemon_spec.rb
@@ -4,7 +4,7 @@ describe "Pokemon" do
   before do
     @db = SQLite3::Database.new(':memory:')
     @db.execute("DROP TABLE IF EXISTS pokemon")
-    @sql_runner = SQLRunner.new(@db)
+    @sql_runner = SQLRunner.new(@db) 
     @sql_runner.execute_schema_migration_sql
   end
 

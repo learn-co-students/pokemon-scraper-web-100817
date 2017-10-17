edbb76c490f4e00c10bc50c72713e09d87bf34eb
MatteBru <mbruni616@gmail.com>
2017-10-17 09:50:50 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,15 @@
 require "pry"
 class Pokemon
 
-  attr_accessor :id, :name, :type, :db, :hp
+  attr_accessor :name, :type, :db, :hp
+  attr_reader :id
 
-  def initialize(id:, name:, type:, db:)
+  def initialize(id:, name:, type:, db:, hp: 60)
     @id = id
     @name = name
     @type = type
     @db = db
+    @hp = hp
   end
 
   def self.save(name, type, db)
@@ -24,11 +26,7 @@ class Pokemon
       SELECT * FROM pokemon WHERE pokemon.id = ?;
     SQL
     poke_arr = db.execute(sql, id).first
-    pokemon = Pokemon.new(id: poke_arr[0], name: poke_arr[1], type: poke_arr[2], db: db)
-    pokemon.hp = poke_arr.first[3]
-    #db.execute("SELECT hp FROM pokemon WHERE pokemon.id = id")[0]
-    pokemon
-    #binding.pry
+    pokemon = Pokemon.new(id: poke_arr[0], name: poke_arr[1], type: poke_arr[2], db: db, hp: poke_arr[3])
   end
 
   def alter_hp(hp, db)
